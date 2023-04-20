977cdc08bf643df00a0d009f16c0f2fe295ed257
Andreas Schneider <andreas.schneider@skillworks.de>
2023-04-20 21:12:58 +0200

    Initial csv adapter version

---
+++ adapter/sma-csv-adapter/src/main/java/dashboard/sma/adapter/csv/SmaCsvAdapterApplication.java
@@ -0,0 +1,11 @@
+package dashboard.sma.adapter.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaCsvAdapterApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SmaCsvAdapterApplication.class, args);
+  }
+}
